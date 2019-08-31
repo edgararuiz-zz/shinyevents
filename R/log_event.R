6381a871a1abfd220d0ece326eb64241b1d05acb
Edgar Ruiz <edgar@rstudio.com>
2019-08-31 18:46:39 -0500

    Adds app name to init

--- R/log_event.R
+++ R/log_event.R
@@ -1,26 +1,32 @@
-
 session_setup <- rlang::new_environment()
 session_setup$init <- FALSE
 session_setup$backend <- NULL
 session_setup$guid <- NULL
+session_setup$app <- NULL
 
 #' @export
-shinyevents_init <- function(backend = c("file", "database", "plumber")) {
+shinyevents_init <- function(app, backend = c("file", "database", "plumber")) {
   session_setup$guid <- uuid::UUIDgenerate()
   session_setup$backend <- backend
+  session_setup$app <- app
   session_setup$init <- TRUE
 }
 
 get_session_uuid <- function() session_setup$guid
-
 get_session_backend <- function() session_setup$backend
+session_init <- function() session_setup$init
 
-get_session_init <- function() session_setup$ini
+#' @export
+get_session_setup <- function() session_setup
 
+#' @export
+shinyevents_log <- function(activity, value) {
+  
+}
 
 event_to_file <- function(sessionid = NULL, app = NULL, 
-                          activity = NULL, value = NULL, time = NULL, 
-                          filename = "shiny-log.csv"){
+                          activity = NULL, value = NULL, 
+                          time = NULL, filename = "shiny-log.csv"){
   cat(paste0(
     sessionid, ",", 
     app, ",",
